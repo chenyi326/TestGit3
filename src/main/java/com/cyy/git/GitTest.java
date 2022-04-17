9ee6ad72952e64835feba0178e3cd0b493538a37
chenyi <2375795852@qq.com>
2022-04-17 19:18:45 +0800

    first commit

---
+++ src/main/java/com/cyy/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.cyy.git;
+
+/**
+ * @author : chenYi
+ * @date: 2022/4/17 - 04 - 17 - 19:14
+ * @Description: com.cyy.git
+ * @version: 1.0
+ */
+public class GitTest {
+    //这是一个main方法,这是一个程序的入口:
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
